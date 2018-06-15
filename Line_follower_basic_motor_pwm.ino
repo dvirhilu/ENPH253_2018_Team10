8bc32cef26767fdf27c1b048ce7939a84bbda3ea
jyarschenko <31526023+jyarschenko@users.noreply.github.com>
2018-06-14 18:32:38 -0700

    Add files via upload

--- Line_follower_basic_motor_pwm.ino
+++ Line_follower_basic_motor_pwm.ino
@@ -3,21 +3,27 @@
 
 int leftDark = 15;
 int rightDark = 15;
-int motorLeft = 3;
-int motorRight = 2;
-double k_p = 0;
-double k_i = 0;
-double k_d = 0;
+int motorLeft = 2;
+int motorRight = 3;
+
+
 int current_time = 0;
 int prev_time = 0;
+
 double error = 0;
 double prev_error = 0;  
+
 double pid;
-int default_speed = 700;
-int integral = 0;
+
+int default_speed = 800;
+
+int gain = 0;
 int derivative = 0;
+int k_p = 0;
+int k_d = 0;
+
 boolean p_disp = false;
-boolean i_disp = false;
+boolean gain_disp = false;
 boolean d_disp = true;
 boolean leftError = true;
 
@@ -29,123 +35,135 @@ void setup(){
 void loop() {
 
   while(!(startbutton())){
-  LCD.clear(); LCD.home();
-  LCD.print("Press Start");
-  delay(100);
-  }
+    LCD.clear(); LCD.home();
+    LCD.print("Press Start");
+    delay(100);
+    }
+  delay(500);
+  while(!(startbutton())) {
 
+     double valuePID = knob(7);
+      k_p = valuePID/6;
+      LCD.setCursor(0,1);
+      LCD.clear();
+      LCD.print(String("K_p:") + k_p);
+      delay(100);
+  }
+  delay(500);
+  while(!(startbutton())) {
+
+     double valuePID = knob(7);
+      k_d = valuePID/6;
+      LCD.setCursor(0,1);
+      LCD.clear();
+      LCD.print(String("K_d:") + k_d);
+      delay(100);
+  }
+  delay(500);
+  
+  while(!(startbutton())) {
+
+     double valuePID = knob(7);
+      leftDark = valuePID/32;
+      LCD.setCursor(0,1);
+      LCD.clear();
+      LCD.print(String("leftDark: ") + leftDark);
+      delay(100);
+  }
+  delay(500);
+
+  while(!(startbutton())) {
+
+     double valuePID = knob(7);
+      rightDark = valuePID/32;
+      LCD.setCursor(0,1);
+      LCD.clear();
+      LCD.print(String("rightDark: ") + rightDark);
+      delay(100);
+  }
+  delay(500);
+
+  while(!(startbutton())) {
+
+     double valuePID = knob(7);
+      default_speed = valuePID;
+      LCD.setCursor(0,1);
+      LCD.clear();
+      LCD.print(String("speeeeed: ") + default_speed);
+      delay(100);
+  }
+  delay(500);
+
+  while(!(startbutton())) {
+
+     double valuePID = knob(7);
+      gain = valuePID/6;
+      LCD.setCursor(0,1);
+      LCD.clear();
+      LCD.print(String("gain:") + gain);
+      delay(100);
+  }
+    delay(500);
+
+    
   
 
-  prev_time = micros();
-  while(!(stopbutton())){
-
-    double choosePID = knob(6);
-    double valuePID = knob(7);
-
-    if(startbutton()){
-      if(d_disp){
-        d_disp = false;
-        p_disp = true;
-      }
-      else if(p_disp){
-        p_disp = false;
-        i_disp = true;
-      }
-      else{
-        i_disp = false;
-        d_disp = true;
-      }
-    }
-    
-    LCD.clear(); LCD.home();
-
-    if(p_disp){
-      k_p = valuePID/8;
+    prev_time = micros();
+    while(!(stopbutton()) && !(startbutton())){
+      int sensorLeft = analogRead(2);
+      int sensorRight = analogRead(3);
+      LCD.clear();
+      LCD.setCursor(0,0); 
+      LCD.print((int)sensorLeft + String(" ") + (int)sensorRight + String(" ") + error);
       LCD.setCursor(0,1);
-      LCD.print("K_p:" + k_p);
-    }
-    else if(i_disp){
-      k_i = valuePID/200;
-      LCD.setCursor(0,1);
-      LCD.print("K_i:" + k_i);
-    }
-    else{
-      k_d = valuePID/200;
-      LCD.setCursor(0,1);
-      LCD.print("K_d:" + k_d);
-    }
 
-    int sensorLeft = analogRead(3);
-    int sensorRight = analogRead(2);
-    LCD.setCursor(0,0); 
-
-    current_time = micros();
-    error = (sensorRight - rightDark) + (leftDark - sensorLeft);
-    if ((sensorRight-rightDark)>5 && (sensorLeft-leftDark)<5){
-      leftError = false;
-    }
-    if ((sensorRight-rightDark)<5 && (sensorLeft-leftDark)>5){
-      leftError = true;
-    }
-    if ((sensorRight-rightDark)>5 && (sensorLeft-leftDark)>5){
-      if(leftError){
-        error -=5;
+      current_time = micros();
+      //error = (sensorRight - rightDark) + (leftDark - sensorLeft);
+      if (sensorRight<rightDark && !(sensorLeft<leftDark)){
+        leftError = false;
+        error = 1;
       }
-      else{
-        error +=5;
+      else if ( !(sensorRight<rightDark) && sensorLeft<leftDark){
+        leftError = true;
+        error = -1;
       }
-    }
-    integral = prev_error + error*(current_time - prev_time);
-    derivative = (error - prev_error)/(current_time - prev_time);
-
-    pid = k_p*error + k_i*integral + k_d*derivative;
-
-    if( pid < 0 ){
-      motor.speed(motorLeft,default_speed);
-      motor.speed(motorRight,default_speed + pid);
-      LCD.print("------>>");
-    }
-    else{
-      motor.speed(motorLeft,default_speed - pid);
-      motor.speed(motorRight,default_speed);
-      LCD.print("<<------");
-    }
-
-    prev_error = error;
-    prev_time = current_time;
-/*
-    if(SensorLeft > LeftDark && SensorRight < RightDark){
-      LCD.print("Turning Left...");
-      motor.speed(motorLeft,700);
-      motor.speed(motorRight, 100);
-    }
-    else{
-      if(SensorLeft > LeftDark && SensorRight > RightDark){
-        LCD.print("Driving Straight...");
-        motor.speed(motorLeft, 700);
-        motor.speed(motorRight, 500);
-      }
-      else{
-        if(SensorLeft < LeftDark && SensorRight > RightDark){
-          LCD.print("Turning Right...");
-          motor.speed(motorLeft, 100);
-          motor.speed(motorRight, 700);
+      else if (sensorRight<rightDark && sensorLeft<leftDark){
+        if(leftError){
+          error = -5;
         }
         else{
-          LCD.print("Off Track...");
-          motor.speed(motorLeft,700);
-          motor.speed(motorRight, 0);
+          error = 5;
         }
       }
-    }*/
+      else{
+        error = 0;
+      }
+      derivative = (error - prev_error)/(current_time - prev_time);
 
-    if(stopbutton()){
-      break;
+      pid = gain * (k_p*error + k_d*derivative);
+
+    
+      if( pid < 0 ){
+        motor.speed(motorLeft,default_speed);
+        motor.speed(motorRight,default_speed + pid);
+        LCD.print(default_speed + String(" ") + (default_speed+pid));
+              
+      }
+      else{
+        motor.speed(motorLeft,default_speed - pid);
+        motor.speed(motorRight,default_speed);
+        LCD.print((default_speed-pid) + String(" ") + (default_speed));
+      }
+      
+
+      prev_error = error;
+      prev_time = current_time;
+      delay(50);
+
+    
     }
-    delay(50);
-  }
 
-  LCD.clear(); LCD.print("Out of Loop");
+  LCD.clear(); LCD.print("reeeeeee");
 
   motor.stop_all();
   delay(100);
