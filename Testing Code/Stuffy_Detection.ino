d12a8d4d4a0a4b18de9be03ce1317a9c595498d8
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-07-05 15:25:19 -0700

    Add files via upload
    [Tested] Short code to pulse the IR LEDs
    - Does not have detection of the stuffy implemented. Only for pulsing
    ** Uses PWM pinout to replace digitalWrite()

---
+++ Testing Code/Stuffy_Detection.ino
@@ -0,0 +1,43 @@
+#include <phys253.h>
+#include <LiquidCrystal.h>
+
+int pulse = 36;
+int zero = 37;
+int start_time;
+int current_time = 0;
+
+void setup() {
+#include <phys253setup.txt>
+  Serial.begin(9600);j 
+  pinMode(pulse, OUTPUT);
+}
+
+void loop() {
+  while (!(startbutton())) {
+    LCD.clear(); LCD.home();
+    LCD.print("Press Start");
+    Serial.println("Press Start");
+    delay(100);
+  }
+  delay(500);
+
+  while (!startbutton()) {
+    LCD.clear();
+    start_time = millis();
+    current_time = millis();
+
+    analogWrite(zero,0);
+
+        while (current_time < 100 + start_time) {
+          analogWrite(pulse, 255);
+          current_time = millis();
+          Serial.println("high");
+        }
+        while (current_time < 200 + start_time) {
+          analogWrite(pulse, 0);
+          current_time = millis();
+          Serial.println("low");
+        }
+
+  }
+}
