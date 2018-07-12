53d06f8bb0a1cff0b7dc527125fbe098f9ff3e50
kotachang <31526993+kotachang@users.noreply.github.com>
2018-07-12 11:33:21 -0700

    Stuffy Detection with Limit Switch
    [Tested] detects stuffy & closes claws & checks if limit switch was pressed. Connection to HK arm servo not added yet

--- Testing Code/Stuffy_Detection.ino
+++ Testing Code/Stuffy_Detection.ino
@@ -1,43 +1,52 @@
-#include <phys253.h>
-#include <LiquidCrystal.h>
-
-int pulse = 36;
-int zero = 37;
-int start_time;
+#include <Servo.h>
+int pulse = 7;
+int switchPin = 8;
+int start_time = 0;
 int current_time = 0;
+int threshold = 400;
+int sensor = 0;
+int readingHigh = 0;
+int readingLow = 0;
+int clawPin = 9;
+int servoAngle = 0;
+Servo claw;
 
 void setup() {
-#include <phys253setup.txt>
-  Serial.begin(9600);j 
+  Serial.begin(9600);
   pinMode(pulse, OUTPUT);
+  pinMode(sensor, INPUT);
+  pinMode(switchPin, INPUT);
+  claw.attach(clawPin);
 }
 
 void loop() {
-  while (!(startbutton())) {
-    LCD.clear(); LCD.home();
-    LCD.print("Press Start");
-    Serial.println("Press Start");
-    delay(100);
-  }
-  delay(500);
-
-  while (!startbutton()) {
-    LCD.clear();
-    start_time = millis();
+  claw.write(35);
+  servoAngle = claw.read();
+  //threshold = menuToggle();
+  //while (!startbutton()) {
+  start_time = millis();
+  current_time = millis();
+  digitalWrite(pulse, HIGH);
+  while (current_time < 100 + start_time) {
     current_time = millis();
-
-    analogWrite(zero,0);
-
-        while (current_time < 100 + start_time) {
-          analogWrite(pulse, 255);
-          current_time = millis();
-          Serial.println("high");
-        }
-        while (current_time < 200 + start_time) {
-          analogWrite(pulse, 0);
-          current_time = millis();
-          Serial.println("low");
-        }
-
+    readingHigh = analogRead(sensor);
+  }
+  digitalWrite(pulse, LOW);
+  while (current_time < 200 + start_time) {
+    current_time = millis();
+    readingLow = analogRead(sensor);
+  }
+  Serial.println(readingHigh + String(" ") + readingLow + String(" ") + (readingHigh - readingLow));
+  if (readingHigh - readingLow > threshold) {
+    Serial.println("detected");
+    claw.write(servoAngle - 20);
+    while (true) {
+      if (digitalRead(switchPin) == 0) {
+        Serial.println("captured");
+        break;
+      }
+    }
+    delay(3000);
+    claw.write(servoAngle);
   }
 }
