ca2986d560d7bb822e52adc2e3fcee28cf2f4677
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-07-14 18:13:53 -0700

    PID library not yet tested

---
+++ libraries/PID/PID.cpp
@@ -0,0 +1,32 @@
+#include "Arduino.h"
+#include "PID.h"
+
+PID::PID( int leftPin, int rightPin, int leftMotorPin, int rightMotorPin ) {
+  pinLeft;
+  pinRight;
+  motorLeft;
+  motorRight;
+  leftDark;
+  rightDark;
+  default_speed;
+  gain;
+  k_p;
+  k_d;
+  prev_time;
+  prev_error;
+}
+
+const char* PID::getName() {
+  return itemName;
+}
+
+int PID::getValue() {
+  return eeprom_read_word(EEPROMAddress);
+}
+void PID::setValue( int value ) {
+  eeprom_write_word(EEPROMAddress, value);
+}
+
+int PID::getEepNum() {
+  return (int)EEPROMAddress;
+}
---
+++ libraries/PID/PID.h
@@ -0,0 +1,38 @@
+#ifndef PID_h
+#define PID_h
+
+#include "Arduino.h"
+
+class PID
+{
+  public:
+    PID( int leftPin, int rightPin, int leftMotorPin, int rightMotorPin );
+    void setKd( int kd );
+    void setKp( int kp);
+    void setGain( int gainz );
+    void setLeftDark( int lDark );
+    void setRightDark( int rDark );
+    void setDefaultSpeed( int motorSpeed );
+    double getPID();
+    void tapeFollow();
+  private:
+    int pinLeft;
+    int pinRight;
+    int motorLeft;
+    int motorRight;
+    int leftDark;
+    int rightDark;
+    int default_speed;
+    int gain;
+    int k_p;
+    int k_d;
+    //double current_time;
+    double prev_time;
+    //double error = 0;
+    double prev_error;
+    //int derivative = 0;
+    //double pid;
+    //boolean leftError = true;
+};
+
+#endif
