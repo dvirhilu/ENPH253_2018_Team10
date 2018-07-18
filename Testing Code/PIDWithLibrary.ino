fe5cec76a6e024a0cd5a273d6f8a367d744d00b3
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-07-17 20:34:01 -0700

    PID Code with the PID Library

---
+++ Testing Code/PIDWithLibrary.ino
@@ -0,0 +1,112 @@
+#include <phys253.h>
+#include <LiquidCrystal.h>
+#include<MenuItem.h>
+#include<avr/EEPROM.h>
+#include <PID.h>
+//PID values
+
+constexpr int motorLeft = 2;
+constexpr int motorRight = 0;
+constexpr int sensorLeftPin = 0;
+constexpr int sensorRightPin = 3;
+constexpr int a_knob_thresh = 100;
+constexpr int s_knob_thresh = 270;
+constexpr int p_knob_thresh = 200;
+constexpr int stuffyComPin = 11;
+
+PID pid( sensorLeftPin, sensorRightPin, motorLeft, motorRight );
+
+MenuItem kp = MenuItem("p_k_p", (unsigned int*)1);
+MenuItem kd = MenuItem("p_k_d", (unsigned int*)5);
+MenuItem gainz = MenuItem("p_gainzz", (unsigned int*)9);
+MenuItem lDark = MenuItem("a_leftDark", (unsigned int*)13);
+MenuItem rDark = MenuItem("a_rightDark", (unsigned int*)17);
+MenuItem motor_speed = MenuItem("m_speeeeed", (unsigned int*)21);
+MenuItem menu[] = {kp, kd, gainz, lDark, rDark, motor_speed};
+
+char analog_sensors = 'a';
+char servos = 's';
+char PID_constants = 'p';
+
+void setup() {
+#include <phys253setup.txt>
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  while ( !startbutton() ) {
+    initialScreen();
+  }
+  delay(500);
+
+  while (!startbutton()) {
+    menuToggle();
+  }
+  delay(500);
+
+  pid.setKp( kp.getValue() );
+  pid.setKd( kd.getValue() );
+  pid.setGain( gainz.getValue() );
+  pid.setLeftDark( lDark.getValue() );
+  pid.setRightDark( rDark.getValue() );
+  pid.setDefaultSpeed( motor_speed.getValue() );
+  pid.initialize();
+
+  LCD.clear(); LCD.home();
+  LCD.print("REEEEEEEEEE");
+  delay(1000);
+
+  while (!(stopbutton()) && !(startbutton())) {
+    pid.tapeFollow();
+  }
+
+  LCD.clear(); LCD.print("reeeeeee");
+  delay(500);
+  motor.stop_all();
+  delay(100);
+}
+
+void menuToggle() {
+  int sizeArray = sizeof(menu[0]);
+  int value = knob(6);
+  int menu_item = knob(7) * (sizeof(menu) / sizeArray) / 1024;
+  if (menu_item > (sizeof(menu) / sizeArray) - 1) {
+    menu_item = sizeof(menu) / sizeArray - 1;
+  }
+  else if (menu_item < 0) {
+    menu_item = 0;
+  }
+  String itemName = (String)menu[menu_item].getName();
+  char firstChar = itemName.charAt(0);
+  LCD.clear(); LCD.home();
+  LCD.print( itemName + " " );
+  LCD.print( (String)menu[menu_item].getValue() + " " );
+  LCD.setCursor(0, 1);
+  if (firstChar == analog_sensors) {
+    LCD.print(a_knob_thresh * (value / 1024.0));
+    if (stopbutton()) menu[menu_item].setValue(a_knob_thresh * (value / 1024.0));
+  }
+  else if (firstChar == servos) {
+    LCD.print(s_knob_thresh * (value / 1024.0));
+    if (stopbutton()) menu[menu_item].setValue(s_knob_thresh * (value / 1024.0));
+  }
+  else if (firstChar == PID_constants) {
+    LCD.print(p_knob_thresh * (value / 1024.0));
+    if (stopbutton()) menu[menu_item].setValue(p_knob_thresh * (value / 1024.0));
+  }
+  else {
+    LCD.print(value);
+    if (stopbutton()) menu[menu_item].setValue(value);
+  }
+  delay(50);
+}
+
+void initialScreen() {
+  LCD.clear(); LCD.home();
+  LCD.print("Press Start");
+  LCD.setCursor(0, 1);
+  LCD.print("for REEEEEEEEE");
+  delay(50);
+}
+
