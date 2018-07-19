f3f0fad711e98cb1426d286d66bffb058f653a6b
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-07-19 16:01:24 -0700

    Updated to take both right and left sides

--- Testing Code/Stuffy_Detection.ino
+++ Testing Code/Stuffy_Detection.ino
@@ -1,69 +1,134 @@
 #include <Servo.h>
 // pins
 
-constexpr int stuffyComPin = 11;
+constexpr int stuffyComPin = 12;
 
-int pulse = 7;
-int switchPin = 8;
-int sensor = 0;
-int clawPin = 9;
-int armPin = 10;
+int pulseLeft = 7;
+int pulseRight = 8;
+int sensorLeft = 0;
+int sensorRight = 1;
+int clawPinLeft = 9;
+int armPinLeft = 10;
+int clawPinRight = 5;
+int armPinRight = 6;
+
 // threshold values
-int threshold = 400; // IR reading threshold
+int threshold = 150; // IR reading threshold
 int clawAngle = 0; // servo starting angle (open position)
-int armAngle = 140; // arm servo starting anlge (lifted position)
+int armAngle = 150; // arm servo starting anlge (lifted position)
 // initialize variables
 int start_time = 0;
 int current_time = 0;
-int readingHigh = 0;
-int readingLow = 0;
-Servo claw;
-Servo arm;
+int readingHighLeft = 0;
+int readingLowLeft = 0;
+int readingHighRight = 0;
+int readingLowRight = 0;
+Servo clawLeft;
+Servo armLeft;
+Servo clawRight;
+Servo armRight;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pulse, OUTPUT);
-  pinMode(sensor, INPUT);
-  pinMode(switchPin, INPUT);
+  pinMode(pulseLeft, OUTPUT);
+  pinMode(sensorLeft, INPUT);
+  pinMode(pulseRight, OUTPUT);
+  pinMode(sensorRight, INPUT);
   pinMode(stuffyComPin, OUTPUT);
-  claw.attach(clawPin);
-  arm.attach(armPin);
+  clawLeft.attach(clawPinLeft);
+  armLeft.attach(armPinLeft);
+  clawRight.attach(clawPinRight);
+  armRight.attach(armPinRight);
 }
 
 void loop() {
-  claw.write(clawAngle);
-  arm.write(armAngle);
-  delay(100);
-  start_time = millis();
-  current_time = millis();
-  digitalWrite(pulse, HIGH);
-  while (current_time < 100 + start_time) {
+  digitalWrite(stuffyComPin, LOW);
+  clawLeft.write(clawAngle);
+  armLeft.write(armAngle);
+  clawRight.write(clawAngle);
+  armRight.write(armAngle);
+  delay(1000);
+  while (true) {
+    start_time = millis();
     current_time = millis();
-    readingHigh = analogRead(sensor);
-  }
-  digitalWrite(pulse, LOW);
-  while (current_time < 200 + start_time) {
-    current_time = millis();
-    readingLow = analogRead(sensor);
-  }
-  Serial.println(readingHigh + String(" ") + readingLow + String(" ") + (readingHigh - readingLow));
-  if (readingHigh - readingLow > threshold) {
-    digitalWrite(stuffyComPin, HIGH);
-    Serial.println("detected");
-    arm.write(20); // change this after testing
-    delay(2000);
-    claw.write(clawAngle + 28);
-    while (true) {
-      if (digitalRead(switchPin) == 0) {
-        Serial.println("captured");
-        break;
-      }
+    digitalWrite(pulseLeft, HIGH);
+    digitalWrite(pulseRight, HIGH);
+    while (current_time < 50 + start_time) {
+      current_time = millis();
+      readingHighLeft = analogRead(sensorLeft);
+      readingHighRight = analogRead(sensorRight);
     }
-    arm.write(200); // angle onto the bin (max angle we can have)
-    delay(2000); // change this timing after testing
-    claw.write(clawAngle);
-    arm.write(armAngle);
-    delay(500);
-    digitalWrite(stuffyComPin, LOW);
+    digitalWrite(pulseLeft, LOW);
+    digitalWrite(pulseRight, LOW);
+    while (current_time < 100 + start_time) {
+      current_time = millis();
+      readingLowLeft = analogRead(sensorLeft);
+      readingLowRight = analogRead(sensorRight);
+    }
+    //Serial.println(readingHigh + String(" ") + readingLow + String(" ") + (readingHigh - readingLow));
+    if (readingHighLeft - readingLowLeft > threshold) {
+      delay(600); // add delay before stopping (sending HIGH to the TINAH)
+      digitalWrite(stuffyComPin, HIGH);
+      Serial.println("detected");
+      armLeft.write(20); // change this after testing
+      /*
+        while (current_time < 400 + start_time) {
+        current_time = millis();
+        }
+      */
+      delay(2000);
+      clawLeft.write(clawAngle + 28);
+      delay(1000);
+      /*
+        while (true) {
+        if (digitalRead(switchPin) == 0) {
+          Serial.println("captured");
+          break;
+        }
+        }
+      */
+      armLeft.write(200);
+      /*while (current_time < 1000 + start_time) {
+        current_time = millis();
+        }*/
+      delay(2000);
+      clawLeft.write(clawAngle);
+      delay(500);
+      armLeft.write(armAngle);
+      delay(1000);
+      digitalWrite(stuffyComPin, LOW);
+    }
+    else if (readingHighRight - readingLowRight > threshold) {
+      delay(600); // add delay before stopping (sending HIGH to the TINAH)
+      digitalWrite(stuffyComPin, HIGH);
+      Serial.println("detected");
+      armRight.write(20); // change this after testing
+      /*
+        while (current_time < 400 + start_time) {
+        current_time = millis();
+        }
+      */
+      delay(2000);
+      clawRight.write(clawAngle + 28);
+      delay(1000);
+      /*
+        while (true) {
+        if (digitalRead(switchPin) == 0) {
+          Serial.println("captured");
+          break;
+        }
+        }
+      */
+      armRight.write(200);
+      /*while (current_time < 1000 + start_time) {
+        current_time = millis();
+        }*/
+      delay(2000);
+      clawRight.write(clawAngle);
+      delay(500);
+      armRight.write(armAngle);
+      delay(1000);
+      digitalWrite(stuffyComPin, LOW);
+    }
   }
 }
