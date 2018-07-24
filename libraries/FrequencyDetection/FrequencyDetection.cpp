b8927e7f43d3b0dcad0fa0c7e4020c23d38810bb
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-07-23 21:12:50 -0700

    Library for Frequency Detection

---
+++ libraries/FrequencyDetection/FrequencyDetection.cpp
@@ -0,0 +1,60 @@
+#include "Arduino.h"
+#include "FrequencyDetection.h"
+
+
+FrequencyDetection::FrequencyDetection( int tenkHz, int onekHz, int tinahPin ) {
+  pin10kHz = tenkHz;
+  pin1kHz = onekHz;
+  tinahComPin = tinahPin;
+  pinMode(pin10kHz, INPUT);
+  pinMode(pin1kHz, INPUT);
+  pinMode(tinahComPin, OUTPUT);
+}
+
+void FrequencyDetection::set10kHzThresh( int thresh ) {
+  thresh10kHz = thresh;
+}
+
+void FrequencyDetection::set1kHzThresh( int thresh ) {
+  thresh1kHz = thresh;
+}
+
+bool FrequencyDetection::is10kHz() {
+  if ( analogRead(pin10kHz) > thresh10kHz ) {
+    return  true;
+  }
+  return false;
+}
+
+bool FrequencyDetection::is1kHz() {
+  if ( analogRead(pin1kHz) > thresh1kHz ) {
+    return  true;
+  }
+  return false;
+}
+
+void FrequencyDetection::handle10kHz() {
+  digitalWrite(tinahComPin, HIGH );
+  while ( !(is1kHz()) ) {}
+  while ( !(is10kHz()) ) {}
+  digitalWrite(tinahComPin, LOW );
+}
+
+void FrequencyDetection::handle1kHz() {
+  digitalWrite(tinahComPin, HIGH);
+  while ( !(is10kHz()) ) {}
+  digitalWrite(tinahComPin, LOW );
+}
+
+void FrequencyDetection::detectFreqency() {
+  while (true) {
+    if ( is1kHz() ) {
+      handle1kHz();
+      break;
+    }
+    else if ( is10kHz() ) {
+      handle10kHz();
+      break;
+    }
+  }
+}
---
+++ libraries/FrequencyDetection/FrequencyDetection.h
@@ -0,0 +1,26 @@
+
+#ifndef FrequencyDetection_h
+#define FrequencyDetection_h
+
+#include "Arduino.h"
+
+class FrequencyDetection
+{
+  public:
+    FrequencyDetection( int tenkHz, int onekHz, int tinahPin );
+    void set10kHzThresh( int thresh );
+    void set1kHzThresh( int thresh );
+    bool is10kHz();
+    bool is1kHz();
+    void handle10kHz();
+    void handle1kHz();
+    void detectFrequency();
+  private:
+    int pin10kHz;
+    int pin1kHz;
+    int tinahComPin;
+    int thresh10kHz;
+    int thresh1kHz;
+};
+
+#endif
