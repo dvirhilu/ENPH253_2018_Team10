ed2105ffaefac6b62243e73843f6ab7d32e15230
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-07-25 15:28:02 -0700

    StuffyCount added [not tested]
    counts number of stuffies pickedup & considers edgeComPin from TINAH to activate left detection system. not tested yet

--- Testing Code/Stuffy_Detection.ino
+++ Testing Code/Stuffy_Detection.ino
@@ -3,6 +3,7 @@
 
 constexpr int stuffyComPin = 12;
 constexpr int count_limit = 2;
+constexpr int edgeComPin = 13;
 
 int pulseLeft = 7;
 int pulseRight = 8;
@@ -26,6 +27,7 @@ int readingHighRight = 0;
 int readingLowRight = 0;
 int countLeft = 0;
 int countRight = 0;
+int stuffyCount = 0;
 Servo clawLeft;
 Servo armLeft;
 Servo clawRight;
@@ -38,6 +40,7 @@ void setup() {
   pinMode(pulseRight, OUTPUT);
   pinMode(sensorRight, INPUT);
   pinMode(stuffyComPin, OUTPUT);
+  pinMode(edgeComPin, INPUT);
   clawLeft.attach(clawPinLeft);
   armLeft.attach(armPinLeft);
   clawRight.attach(clawPinRight);
@@ -48,46 +51,55 @@ void loop() {
   digitalWrite(stuffyComPin, LOW);
   clawLeft.write(clawAngle);
   armLeft.write(armAngle);
-  clawRight.write(24);
+  clawRight.write(25);
   armRight.write(200 - armAngle);
   delay(1000);
   while (true) {
     start_time = millis();
     current_time = millis();
-    digitalWrite(pulseLeft, HIGH);
-    digitalWrite(pulseRight, HIGH);
-    while (current_time < 5 + start_time) {
-      current_time = millis();
-      readingHighLeft = analogRead(sensorLeft);
-      readingHighRight = analogRead(sensorRight);
+    if (stuffyCount < 3) {
+      digitalWrite(pulseRight, HIGH);
+      while (current_time < 5 + start_time) {
+        current_time = millis();
+        readingHighRight = analogRead(sensorRight);
+      }
+      digitalWrite(pulseRight, LOW);
+      while (current_time < 10 + start_time) {
+        current_time = millis();
+        readingLowRight = analogRead(sensorRight);
+      }
+      if (readingHighRight - readingLowRight > threshold) {
+        countRight++;
+      }
+      else {
+        countRight = 0;
+      }
+      if ( countRight == count_limit ) {
+        rightDetection();
+        countRight = 0;
+      }
     }
-    digitalWrite(pulseLeft, LOW);
-    digitalWrite(pulseRight, LOW);
-    while (current_time < 10 + start_time) {
-      current_time = millis();
-      readingLowLeft = analogRead(sensorLeft);
-      readingLowRight = analogRead(sensorRight);
-    }
-    Serial.println(readingHighRight + String(" ") + readingLowRight + String(" ") + (readingHighRight - readingLowRight));
-    if (readingHighLeft - readingLowLeft > threshold) {
-      countLeft++;
-    }
-    else {
-      countLeft = 0;
-    }
-    if (readingHighRight - readingLowRight > threshold) {
-      countRight++;
-    }
-    else {
-      countRight = 0;
-    }
-    if ( countLeft == count_limit ) {
-      leftDetection();
-      countLeft = 0;
-    }
-    if ( countRight == count_limit ) {
-      rightDetection();
-      countRight = 0;
+    else if (digitalRead(edgeComPin) == HIGH) {
+      digitalWrite(pulseLeft, HIGH);
+      while (current_time < 5 + start_time) {
+        current_time = millis();
+        readingHighLeft = analogRead(sensorLeft);
+      }
+      digitalWrite(pulseLeft, LOW);
+      while (current_time < 10 + start_time) {
+        current_time = millis();
+        readingLowLeft = analogRead(sensorLeft);
+      }
+      if (readingHighLeft - readingLowLeft > threshold) {
+        countLeft++;
+      }
+      else {
+        countLeft = 0;
+      }
+      if ( countLeft == count_limit ) {
+        leftDetection();
+        countLeft = 0;
+      }
     }
   }
 }
@@ -97,26 +109,10 @@ void leftDetection() {
   delay(600); // add delay before stopping (sending HIGH to the TINAH)
   Serial.println("detected");
   armLeft.write(20); // change this after testing
-  /*
-    while (current_time < 400 + start_time) {
-    current_time = millis();
-    }
-  */
   delay(2000);
   clawLeft.write(clawAngle + 28);
   delay(1000);
-  /*
-    while (true) {
-    if (digitalRead(switchPin) == 0) {
-      Serial.println("captured");
-      break;
-    }
-    }
-  */
-  armLeft.write(200);
-  /*while (current_time < 1000 + start_time) {
-    current_time = millis();
-    }*/
+  armLeft.write(190);
   delay(2000);
   clawLeft.write(clawAngle);
   delay(500);
@@ -127,35 +123,27 @@ void leftDetection() {
 
 
 void rightDetection() {
-  digitalWrite(stuffyComPin, HIGH);
-  delay(600); // add delay before stopping (sending HIGH to the TINAH)
-  Serial.println("detected");
-  armRight.write(200); // change this after testing
-  /*
-    while (current_time < 400 + start_time) {
-    current_time = millis();
-    }
-  */
-  delay(2000);
-  clawRight.write(clawAngle);
-  delay(1000);
-  /*
-    while (true) {
-    if (digitalRead(switchPin) == 0) {
-      Serial.println("captured");
-      break;
-    }
-    }
-  */
-  armRight.write(20);
-  /*while (current_time < 1000 + start_time) {
-    current_time = millis();
-    }*/
-  delay(2000);
-  clawRight.write(24);
-  delay(500);
-  armRight.write(200 - armAngle);
-  delay(1000);
-  digitalWrite(stuffyComPin, LOW);
+  if (stuffyCount < 2) {
+    digitalWrite(stuffyComPin, HIGH);
+    delay(600); // add delay before stopping (sending HIGH to the TINAH)
+    Serial.println("detected");
+    armRight.write(195); // change this after testing
+    delay(2000);
+    clawRight.write(clawAngle);
+    delay(1000);
+    armRight.write(30);
+    delay(2000);
+    clawRight.write(25);
+    delay(500);
+    armRight.write(200 - armAngle);
+    delay(1000);
+    stuffyCount++;
+    digitalWrite(stuffyComPin, LOW);
+  }
+  else if (stuffyCount == 3) {
+    digitalWrite(stuffyComPin, HIGH);
+    delay(20);
+    digitalWrite(stuffyComPin, LOW);
+  }
 }
 
