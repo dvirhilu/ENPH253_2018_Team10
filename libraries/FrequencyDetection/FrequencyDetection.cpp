7f79ee458cc3cc0371ab05d8dc9d4281db94979f
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-08-06 07:03:55 -0700

    Updated as of August 6

--- libraries/FrequencyDetection/FrequencyDetection.cpp
+++ libraries/FrequencyDetection/FrequencyDetection.cpp
@@ -20,6 +20,7 @@ void FrequencyDetection::set1kHzThresh( int thresh ) {
 }
 
 bool FrequencyDetection::is10kHz() {
+	Serial.print( analogRead(pin10kHz) + String(" 10K ") );
   if ( analogRead(pin10kHz) > thresh10kHz ) {
     return  true;
   }
@@ -27,6 +28,7 @@ bool FrequencyDetection::is10kHz() {
 }
 
 bool FrequencyDetection::is1kHz() {
+	Serial.print(analogRead(pin1kHz) + String(" 1K "));
   if ( analogRead(pin1kHz) > thresh1kHz ) {
     return  true;
   }
@@ -34,26 +36,31 @@ bool FrequencyDetection::is1kHz() {
 }
 
 void FrequencyDetection::handle10kHz() {
-  digitalWrite(tinahComPin, HIGH );
-  while ( !(is1kHz()) ) {}
+ 
+while ( !(is1kHz()) ) { digitalWrite(LED_BUILTIN, HIGH); }
+digitalWrite(LED_BUILTIN, LOW);
   while ( !(is10kHz()) ) {}
-  digitalWrite(tinahComPin, LOW );
 }
 
 void FrequencyDetection::handle1kHz() {
-  digitalWrite(tinahComPin, HIGH);
-  while ( !(is10kHz()) ) {}
-  digitalWrite(tinahComPin, LOW );
+  
+  while ( !(is10kHz()) ) { digitalWrite(LED_BUILTIN, HIGH);}
+  digitalWrite(LED_BUILTIN,LOW);
 }
 
 void FrequencyDetection::detectFrequency() {
+  digitalWrite(tinahComPin, HIGH);
   while (true) {
     if ( is1kHz() ) {
+		Serial.println("it's 1K");
       handle1kHz();
+	  digitalWrite(tinahComPin, LOW );
       break;
     }
     else if ( is10kHz() ) {
+		Serial.println("it's 10K");
       handle10kHz();
+	  digitalWrite(tinahComPin, LOW );
       break;
     }
   }
