6666648bef51e31e405c0b6308a0289594b0423c
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-08-07 18:55:35 -0700

    Add files via upload

---
+++ Testing Code/HbridgeTesting.ino
@@ -0,0 +1,21 @@
+#include <phys253.h>
+#include <LiquidCrystal.h>
+
+int motorPin = 0;
+int knobVal = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+#include <phys253setup.txt>
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  knobVal = knob(6) - 512;
+  motor.speed(motorPin,knobVal);
+  LCD.clear(); LCD.home();
+  LCD.print( knobVal);
+  delay(50);
+}
