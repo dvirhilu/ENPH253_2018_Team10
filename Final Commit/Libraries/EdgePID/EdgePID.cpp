821b691df0dbc159952c78ffd285ca385c656fba
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-08-09 00:11:46 -0700

    Add files via upload

---
+++ Final Commit/Libraries/EdgePID/EdgePID.cpp
@@ -0,0 +1,54 @@
+#include "Arduino.h"
+#include "EdgePID.h"
+
+EdgePID::EdgePID( int leftPin, int rightPin, int leftMotorPin, int rightMotorPin ) {
+  pinLeft = leftPin;
+  pinRight = rightPin;
+  motorLeft = leftMotorPin;
+  motorRight = rightMotorPin;
+  default_speed = 0;
+  k_p = 0;
+}
+void EdgePID::setEdgeKp( int kp ) {
+  k_p = kp;
+}
+
+void EdgePID::setDefaultSpeed( int motorSpeed ) {
+  default_speed = motorSpeed;
+}
+
+double EdgePID::getEdgePID() {
+  int sensorLeft = analogRead(pinLeft);
+  int sensorRight = analogRead(pinRight);
+  double error = 0;
+  double pid;
+
+  if ( sensorLeft > LEFT_REKT ) {
+    error = 3;
+  }
+  else if ( sensorRight < RIGHT_REKT ) {
+    error = -3;
+  }
+  else if ( sensorLeft > LEFT_THRESH ) {
+    error = 1;
+  }
+  else if ( sensorRight < RIGHT_THRESH ) {
+    error = -1;
+  }
+  else {
+    error = 0;
+  }
+
+  pid = k_p * error;
+
+  return pid;
+}
+
+void EdgePID::edgeFollow() {
+
+  double pid = getEdgePID();
+
+  motor.speed(motorLeft, default_speed - 0.5 * pid);
+  motor.speed(motorRight, default_speed + 0.5 * pid);
+}
+
---
+++ Final Commit/Libraries/EdgePID/EdgePID.h
@@ -0,0 +1,30 @@
+#ifndef EdgePID_h
+#define EdgePID_h
+
+#include "Arduino.h"
+#include "phys253.h"
+#include "LiquidCrystal.h"
+
+#define LEFT_THRESH 300
+#define LEFT_REKT 400
+#define RIGHT_THRESH 200
+#define RIGHT_REKT 100
+
+class EdgePID
+{
+  public:
+    EdgePID( int leftPin, int rightPin, int leftMotorPin, int rightMotorPin );
+    void setEdgeKp( int kp );
+    void setDefaultSpeed( int motorSpeed );
+    double getEdgePID();
+    void edgeFollow();
+  private:
+    int pinLeft;
+    int pinRight;
+    int motorLeft;
+    int motorRight;
+    int default_speed;
+    int k_p;
+};
+
+#endif
