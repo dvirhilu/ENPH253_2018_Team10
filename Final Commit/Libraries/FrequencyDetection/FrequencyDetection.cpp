b7cdcf6a7f2512f95af85630aa19c66830ca8bb8
dvirhilu10 <40151041+dvirhilu10@users.noreply.github.com>
2018-08-09 00:10:11 -0700

    Add files via upload

---
+++ Final Commit/Libraries/FrequencyDetection/FrequencyDetection.cpp
@@ -0,0 +1,73 @@
+#include "Arduino.h"
+#include "FrequencyDetection.h"
+
+
+FrequencyDetection::FrequencyDetection( int tenkHz, int onekHz, int tinahPin ) {
+  pin10kHz = tenkHz;
+  pin1kHz = onekHz;
+  tinahComPin = tinahPin;
+  pinMode(pin10kHz, INPUT);
+  pinMode(pin1kHz, INPUT);
+  pinMode(tinahComPin, OUTPUT);
+}
+
+void FrequencyDetection::set10kHzThresh( int thresh ) {
+  thresh10kHz = thresh;
+}
+
+void FrequencyDetection::set1kHzThresh( int thresh ) {
+  thresh1kHz = thresh;
+}
+
+bool FrequencyDetection::is10kHz() {
+	if( analogRead(pin10kHz) == 0 ){
+		return false;
+	}
+	Serial.print( analogRead(pin10kHz) + String(" 10K ") );
+  if ( analogRead(pin1kHz) / analogRead(pin10kHz) < 3 ) {
+    return  true;
+  }
+  return false;
+}
+
+bool FrequencyDetection::is1kHz() {
+	Serial.print(analogRead(pin1kHz) + String(" 1K "));
+	if( analogRead( pin10kHz ) == 0 ){
+		return true;
+	}
+  if ( analogRead(pin1kHz) / analogRead(pin10kHz) >= 3 ) {
+    return  true;
+  }
+  return false;
+}
+
+void FrequencyDetection::handle10kHz() {
+ 
+while ( !(is1kHz()) ) { digitalWrite(LED_BUILTIN, HIGH); }
+digitalWrite(LED_BUILTIN, HIGH);
+  while ( !(is10kHz()) ) {}
+}
+
+void FrequencyDetection::handle1kHz() {
+  
+  while ( !(is10kHz()) ) { digitalWrite(LED_BUILTIN, HIGH);}
+  digitalWrite(LED_BUILTIN,HIGH);
+}
+
+void FrequencyDetection::detectFrequency() {
+  digitalWrite(tinahComPin, HIGH);
+  while (true) {
+    if ( is1kHz() ) {
+		Serial.println("it's 1K");
+      handle1kHz();
+	  digitalWrite(tinahComPin, LOW );
+      break;
+    }
+    else if ( is10kHz() ) {
+		Serial.println("it's 10K");
+      handle10kHz();
+	  digitalWrite(tinahComPin, LOW );
+      break;
+    }
+  }
+}
